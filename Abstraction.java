a0f3e5cb1461b9f0978d8962b5437272699c29bc
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2023-09-13 14:21:24 +0800

    Create Abstraction.java

---
+++ Abstraction.java
@@ -0,0 +1,10 @@
+import ClassPack.ClassAbstraction.*;
+public class Abstraction {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.sound();
+
+        Dog myDog = new Dog();
+        myDog.sound();
+    }
+}
